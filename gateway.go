1bd3e77e70bb4ae4584d503d7bbd131f625da9b1
jimber <jimber@MacBook-Pro.localdomain>
2023-01-21 03:47:34 -0800

    adding discoverinterfaceip os independent + discoverinterface for raw net interface

--- gateway.go
+++ gateway.go
@@ -18,6 +18,52 @@ func DiscoverGateway() (ip net.IP, err error) {
 }
 
 // DiscoverInterface is the OS independent function to call to get the default network interface IP that uses the default gateway
-func DiscoverInterface() (ip net.IP, err error) {
-	return discoverGatewayInterfaceOSSpecific()
+func DiscoverInterfaceIp() (ip net.IP, err error) {
+	ipOfGateway, err1 := discoverGatewayOSSpecific()
+	if err1 != nil {
+		return nil, err1
+	}
+	allInts, _ := net.Interfaces()
+	for _, netInt := range allInts {
+		addrs, _ := netInt.Addrs()
+		for _, addr := range addrs {
+
+			ipA, ipnetA, err2 := net.ParseCIDR(addr.String())
+			if err2 != nil {
+				return nil, err1
+			}
+			if ipnetA.Contains(ipOfGateway) {
+				return ipA, nil
+
+			}
+
+		}
+	}
+	return nil, errors.New("Could not find interface")
+
+}
+
+func DiscoverInterface() (netInterface *net.Interface, err error) {
+	ipOfGateway, err1 := discoverGatewayOSSpecific()
+	if err1 != nil {
+		return nil, err1
+	}
+	allInts, _ := net.Interfaces()
+	for _, netInt := range allInts {
+		addrs, _ := netInt.Addrs()
+		for _, addr := range addrs {
+
+			_, ipnetA, err2 := net.ParseCIDR(addr.String())
+			if err2 != nil {
+				return nil, err1
+			}
+			if ipnetA.Contains(ipOfGateway) {
+				return &netInt, nil
+
+			}
+
+		}
+	}
+	return nil, errors.New("Could not find interface")
+
 }
